dadc9a11f58cbee362cd17814b351c80c9448194
Tai Shi Ling <shiling.tai@gmail.com>
2014-04-20 13:51:25 +0800

    Change db address to localhost default port for mongodb

--- models/db.js
+++ models/db.js
@@ -8,8 +8,8 @@ var ThemeProvider = require('./ThemeProvider.js');
 var ImageProvider = require('./ImageProvider.js');
 
 //db variables
-//var dbUrl = "mongodb://localhost:27017/first";
-var dbUrl = "mongodb://nodejitsu:ae581d34b926f1ac7c6683e53903d14a@alex.mongohq.com:10033/nodejitsudb5932145228";
+var dbUrl = "mongodb://localhost:27017/first";
+//var dbUrl = "mongodb://nodejitsu:ae581d34b926f1ac7c6683e53903d14a@alex.mongohq.com:10033/nodejitsudb5932145228";
 
 //connect to db
 mongoClient.connect(dbUrl, function(err, db){
